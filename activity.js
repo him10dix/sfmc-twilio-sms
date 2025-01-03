8197073d09046e5fcd6d657f479799b01e491b53
him10dix <112629715+him10dix@users.noreply.github.com>
2025-01-03 21:50:44 +0530

    Update activity.js

--- activity.js
+++ activity.js
@@ -19,5 +19,56 @@ connection.on('clickedNext', function(){
 	
 });
 
+// hiddenconfiguration
+
+// var connection = new Postmonger.Session();
+
+// // Startup Sequence
+// connection.trigger('ready');
+// connection.on('initActivity', function(data){
+  
+// 	// document.getElementById('hiddenconfiguration').value = JSON.stringify(data, null, 2);
+// 	document.getElementById('configuration').value = 'Hello Paste the JSON HERE';
+// 	console.log("in initActivity")
+	
+	
+// });	
+	
+// // Save Sequence
+// connection.on('clickedNext', function(){
+//     console.log("in clickedNext")
+// 	// var hiddenconfiguration = JSON.parse(document.getElementById('hiddenconfiguration').value);
+
+//     // hiddenconfiguration['metaData'] = {
+
+//     //     "icon": "https://demo.genetrix.tech/IVRCall/icon.png",
+    
+//     //     "category": "message",
+    
+//     //     "iconSmall": "",
+    
+//     //     "original_icon": "icon.png",
+    
+//     //     "isConfigured": true
+    
+//     //   };
+
+//     var configuration = JSON.parse(document.getElementById('configuration').value);
 
 
+//     // configuration = {
+//     //     "inArguments": [
+//     //       {"contactKey": "{{Contact.Key}}" },
+//     //       {"TargetType": "{{Contact.Attribute.JourneyEntrySource.TargetType}}"},
+//     //       {"TargetId"  : "{{Contact.Attribute.JourneyEntrySource.TargetId}}"},
+//     //       {"PhoneNumber" : "{{Contact.Attribute.JourneyEntrySource.PhoneNumber}}"}
+//     //     ],
+//     //     "url": "https://demo.genetrix.tech/IVRCall/MakeCall.php"
+//     //   };
+    
+//     // hiddenconfiguration['arguments'] = configuration;
+
+// 	console.log()
+// 	connection.trigger('updateActivity', hiddenconfiguration);
+	
+// });
